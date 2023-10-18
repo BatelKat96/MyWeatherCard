af9b5518f001b125c6ee431e7b3ff8835fb540ce
BatelKat96 <batelkat@gmail.com>
2023-10-18 23:09:09 +0300

    Create a basic cities selector

--- force-app/main/default/lwc/citiesSelector/citiesSelector.js
+++ force-app/main/default/lwc/citiesSelector/citiesSelector.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+const cities = ['New York', 'Los Angeles', 'Chicago', 'San Francisco'];
+export default class CitiesSelector extends LightningElement {
+    @track selectedCity = ''
+    showDropdown = false;
+    city = ''
 
-export default class CitiesSelector extends LightningElement {}
+    get filteredCities() {
+        return cities.filter(city => city.toLowerCase().includes(this.selectedCity.toLowerCase()));
+    }
+
+    handleChange(event) {
+        this.selectedCity = event.target.value
+        this.showDropdown = true;
+
+    }
+
+    selectCity(event) {
+        this.selectedCity = event.target.textContent;
+        this.showDropdown = false;
+        this.city = event.target.textContent
+    }
+
+    changeDropdown() {
+        this.showDropdown = true;
+    }
+
+}
