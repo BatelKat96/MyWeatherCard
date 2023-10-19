be617af554d7c4b4db1b1035d05a29dfd7c67a13
BatelKat96 <batelkat@gmail.com>
2023-10-19 17:42:08 +0300

    Create weather details cmp with hard coded place

--- force-app/main/default/lwc/weatherDetails/weatherDetails.js
+++ force-app/main/default/lwc/weatherDetails/weatherDetails.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getAPI from "@salesforce/apex/GetWeatherAPI.getAPI"
 
-export default class WeatherDetails extends LightningElement {}
+export default class WeatherDetails extends LightningElement {
+
+    @track weather
+    apiKey
+
+    @wire(getAPI)
+    wiredAPI({ error, data }) {
+        if (data) {
+            this.apiKey = data
+            this.getWeather()
+        } else if (error) {
+            this.weather = 'Can\'t get weather try to refresh'
+        }
+    }
+
+    async getWeather() {
+        try {
+            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=London&aqi=yes`);
+            const data = await response.json();
+            const { current, location } = data
+            const weatherInfo = {
+                city: location.name,
+                country: location.country,
+                time: current.last_updated,
+                temp: current.temp_c,
+                imgUrl: current.condition.icon,
+                desc: current.condition.text,
+            }
+            this.weather = weatherInfo
+        } catch (err) {
+            this.weather = 'Can\'t get weather try to refresh'
+        }
+    }
+
+}
