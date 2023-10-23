8a27d74270c3d1c23ca8451dbbce70c5577c5b97
BatelKat96 <batelkat@gmail.com>
2023-10-23 23:37:14 +0300

    Create caching for weather API

--- force-app/main/default/lwc/weatherDetails/weatherDetails.js
+++ force-app/main/default/lwc/weatherDetails/weatherDetails.js
@@ -1,14 +1,12 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import getAPI from "@salesforce/apex/GetWeatherAPI.getAPI"
-
+const STORAGE_KEY = 'weather_db'
 export default class WeatherDetails extends LightningElement {
-
     @track weather
     apiKey
     _city
     error
     @track msg = 'Pick a city to see the weather'
-
 
     @api get choosencity() {
         return this._city;
@@ -31,31 +29,49 @@ export default class WeatherDetails extends LightningElement {
 
     async getWeather() {
         if (!this._city) return
-        try {
-            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this._city}&aqi=yes`);
-            const data = await response.json();
-            const { current, location } = data
-            const weatherInfo = {
-                city: location.name,
-                country: location.country,
-                lastUpdate: Date.now(),
-                time: this.convertTime(location.localtime),
-                temp: current.temp_c,
-                imgUrl: current.condition.icon,
-                desc: current.condition.text,
+
+        const weathers = this._loadFromStorage()
+
+        if (weathers[this._city]) {
+            this.weather = weathers[this._city]
+        } else {
+            try {
+                const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this._city}&aqi=yes`);
+                const data = await response.json();
+                const { current, location } = data
+                const weatherInfo = {
+                    city: location.name,
+                    country: location.country,
+                    lastUpdate: Date.now(),
+                    time: this._convertTime(location.localtime),
+                    temp: current.temp_c,
+                    imgUrl: current.condition.icon,
+                    desc: current.condition.text,
+                }
+                weathers[this._city] = weatherInfo
+                this._saveToStorage(weathers)
+                this.weather = weatherInfo
+            } catch (err) {
+                this.msg = 'Can\'t get weather try to refresh'
             }
-            this.weather = weatherInfo
-        } catch (err) {
-            this.msg = 'Can\'t get weather try to refresh'
         }
     }
 
-    convertTime(localTime) {
+    _convertTime(localTime) {
         const time = localTime.slice(11)
         let date = localTime.slice(0, 10)
         date = date.replaceAll('-', '.')
         date = date.split('.').reverse().join('.')
-
         return `${time}, ${date}`
     }
+
+    _saveToStorage(value) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(value))
+    }
+
+    _loadFromStorage() {
+        const data = localStorage.getItem(STORAGE_KEY)
+        return (data) ? JSON.parse(data) : {}
+    }
+
 }
